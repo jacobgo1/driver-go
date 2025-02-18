8b1156c041b58c4f95152d3bb766ea7be202aef9
jacobgo1 <jacob.golsson@gmail.com>
2025-02-18 16:53:22 +0100

    Refactor elevator control logic to implement a queue for handling floor requests

--- main.go
+++ main.go
@@ -1,86 +1,130 @@
 package main
 
 import (
-    "Driver-go/elevio"
-    "fmt"
+	"Driver-go/elevio"
+	"fmt"
+	"time"
 )
+
+type Queue struct {
+	orders []int
+}
+
+func (q *Queue) Enqueue(floor int) {
+	q.orders = append(q.orders, floor)
+}
+
+func (q *Queue) Dequeue() int {
+	if len(q.orders) == 0 {
+		return -1
+	}
+	floor := q.orders[0]
+	q.orders = q.orders[1:]
+	return floor
+}
+
+func (q *Queue) Peek() int {
+	if len(q.orders) == 0 {
+		return -1
+	}
+	return q.orders[0]
+}
+
+func (q *Queue) sneak(floor int) {
+	q.orders = append([]int{floor}, q.orders...)
+}
 
 func main() {
 
-    numFloors := 9
+	numFloors := 4
 
-    elevio.Init("localhost:12345", numFloors)
+	elevio.Init("localhost:15657", numFloors)
 
-    var d elevio.MotorDirection = elevio.MD_Stop
-    elevio.SetMotorDirection(d)
+	var d elevio.MotorDirection = elevio.MD_Stop
+	var currentFloor int = -1
 
-    drv_buttons := make(chan elevio.ButtonEvent)
-    drv_floors := make(chan int)
-    drv_obstr := make(chan bool)
-    drv_stop := make(chan bool)
-    var target int = 0
-    var currentFloor int = 0
-    var moving bool = false
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
 
-    go elevio.PollButtons(drv_buttons)
-    go elevio.PollFloorSensor(drv_floors)
-    go elevio.PollObstructionSwitch(drv_obstr)
-    go elevio.PollStopButton(drv_stop)
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+	
+	queue := Queue{}
 
-    for {
-        select {
-        case a := <-drv_buttons:
-            fmt.Printf("%+v\n", a)
-            elevio.SetButtonLamp(a.Button, a.Floor, true)
-            if !moving {
-                target = a.Floor
-                moving = true
-                fmt.Printf("Target is set to %d\n", target)
-                fmt.Printf("Floor is %d\n", currentFloor)
-                if a.Floor == currentFloor {
-                    elevio.SetMotorDirection(elevio.MD_Stop)
-                    fmt.Printf("Stopped")
-                    moving = false
-                } else if a.Floor > currentFloor {
-                    elevio.SetMotorDirection(elevio.MD_Up)
-                    fmt.Printf("Going up")
-                } else {
-                    elevio.SetMotorDirection(elevio.MD_Down)
-                    fmt.Printf("Going down")
-                }
-            }
+	for {
+		select {
+		case a := <-drv_buttons:
+			fmt.Printf("%+v\n", a)
+			elevio.SetButtonLamp(a.Button, a.Floor, true)
+			//queue.Enqueue(a.Floor)
+			if a.Button == elevio.BT_HallUp || a.Button == elevio.BT_HallDown {
+				queue.Enqueue(a.Floor)
+			} else if a.Button == elevio.BT_Cab {
+				queue.sneak(a.Floor)
+			}
+			targetFloor := queue.Peek()
+			if targetFloor > currentFloor {
+				d = elevio.MD_Up
+			} else if targetFloor < currentFloor {
+				d = elevio.MD_Down
+			} else {
+				d = elevio.MD_Stop
+				elevio.SetButtonLamp(elevio.BT_HallUp, currentFloor, false)
+				elevio.SetButtonLamp(elevio.BT_HallDown, currentFloor, false)
+				elevio.SetButtonLamp(elevio.BT_Cab, currentFloor, false)
+			}
+			elevio.SetMotorDirection(d)
 
-        case a := <-drv_floors:
-            fmt.Printf("%+v\n", a)
-            currentFloor = a
+		case a := <-drv_floors:
+			fmt.Printf("%+v\n", a)
+			currentFloor = a
+			if currentFloor == queue.Peek() {
+				d = elevio.MD_Stop
+				elevio.SetMotorDirection(d)
 
-            if a == target {
-                elevio.SetMotorDirection(elevio.MD_Stop)
-                fmt.Printf("Reached target")
-                moving = false
-            } else if a < target {
-                elevio.SetMotorDirection(elevio.MD_Up)
-                fmt.Printf("still going up")
-            } else {
-                elevio.SetMotorDirection(elevio.MD_Down)
-                fmt.Printf("still going down")
-            }
+				elevio.SetButtonLamp(elevio.BT_HallUp, currentFloor, false)
+				elevio.SetButtonLamp(elevio.BT_HallDown, currentFloor, false)
+				elevio.SetButtonLamp(elevio.BT_Cab, currentFloor, false)
+				// wait for 3 seconds
+				elevio.SetDoorOpenLamp(true)
+				elevio.SetFloorIndicator(currentFloor)
+				time.Sleep(3 * time.Second)
+				elevio.SetDoorOpenLamp(false)
 
-        case a := <-drv_obstr:
-            fmt.Printf("%+v\n", a)
-            if a {
-                elevio.SetMotorDirection(elevio.MD_Stop)
-            } else {
-                elevio.SetMotorDirection(d)
-            }
+				queue.Dequeue()
+				if len(queue.orders) > 0 {
+					targetFloor := queue.Peek()
+					if targetFloor > currentFloor {
+						d = elevio.MD_Up
+					} else if targetFloor < currentFloor {
+						d = elevio.MD_Down
+					} else {
+						d = elevio.MD_Stop
+					}
+					elevio.SetMotorDirection(d)
+				}
+			}
 
-        case a := <-drv_stop:
-            fmt.Printf("%+v\n", a)
-            for f := 0; f < numFloors; f++ {
-                for b := elevio.ButtonType(0); b < 3; b++ {
-                    elevio.SetButtonLamp(b, f, false)
-                }
-            }
-        }
-    }
+		case a := <-drv_obstr:
+			fmt.Printf("%+v\n", a)
+			if a {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			} else {
+				elevio.SetMotorDirection(d)
+			}
+
+		case a := <-drv_stop:
+			fmt.Printf("%+v\n", a)
+			for f := 0; f < numFloors; f++ {
+				for b := elevio.ButtonType(0); b < 3; b++ {
+					elevio.SetButtonLamp(b, f, false)
+				}
+			}
+			queue = Queue{} // Clear the queue
+		}
+	}
 }
